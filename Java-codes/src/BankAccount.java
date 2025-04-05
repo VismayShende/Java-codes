fef7fb39f43a38a8a1392f183f82f9ea1bf0414c
VismayShende <shendevismay@gmail.com>
2025-04-05 15:42:25 +0530

    BankingAccount

---
+++ Java-codes/src/BankAccount.java
@@ -0,0 +1,23 @@
+import java.util.scanner;
+public class BankAccount {
+ String accountHolder;
+   String accountNumber;
+   Double balance;
+   Bank Account (String acc,String acco, double bal){
+ accountHolder=acc;
+ accountNumber=acco;
+balance=bal;
+   }
+Public void deposit(double ammount){
+    if (amount>0){
+        balance+=amount;
+        System.out.println(deposits"+balance");
+    }
+    else{
+        System.out.println/invalid ammount");
+        
+
+    }
+}
+
+}
