bc63782d6a8902dae2d3ba793606d6384b99a22a
VismayShende <shendevismay@gmail.com>
2025-04-07 00:06:11 +0530

    account

---
+++ Java-codes/src/Bank.java
@@ -0,0 +1,24 @@
+class Account {
+    private double balance = 0;
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class Bank {
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.deposit(1000);
+        acc.withdraw(300);
+        System.out.println("Balance: $" + acc.getBalance());
+    }
+}
