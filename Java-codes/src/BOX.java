f3ff96328eb51eaffdc7b2525b71155eea91ffc8
VismayShende <shendevismay@gmail.com>
2025-04-07 00:53:58 +0530

    box

---
+++ Java-codes/src/BOX.java
@@ -0,0 +1,12 @@
+// Try-Finally without Catch
+
+
+public class BOX {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Inside try");
+        } finally {
+            System.out.println("Always executes finally");
+        }
+    }
+}
